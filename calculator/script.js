c6a981fa597c0c6ac38d9a6caf7c2744229d69a3
CodingDashUU <washutshotheli8@gmail.com>
2025-10-02 17:18:06 +0200

    Added Error Handling and Comments

--- calculator/script.js
+++ calculator/script.js
@@ -1,32 +1,43 @@
-function ShowNumber(...value){
+//Display the equation on the screen
+function ShowNumber(...value){  
      let answer = document.getElementById("answer");
      answer.textContent += value;
 }
- let exp = "";
-     function setback(){
+ //Reset the screen and font size
+let exp = "";
+let ans2 = document.getElementById("answer");
+     function setback(){ 
         exp = "";
        document.getElementById("answer").textContent = ""
        document.getElementById("answer").style.fontSize = "5em";
 }
-
-function onclick(id,number){
+//Handles clicking events of the buttons and calculations
+function onclick(id,number){  
     document.getElementById(id).onclick = function(){
       
      exp += number;
      ShowNumber(number);
      document.getElementById("equal").onclick = function(){
+        try{
          let fial = eval(exp);
+         //Handles number/0
           if(fial == "Infinity"){
-           let ans2 = document.getElementById("answer");
            ans2.textContent = "Your divisor may not be 0";
            ans2.style.fontSize = "29px";
            setTimeout(setback,5000)
         }
           else{
-           document.getElementById("answer").textContent = fial;}}
+           document.getElementById("answer").textContent = fial;
            }
-
     }
+    catch(error){
+       document.getElementById("answer").textContent = error;
+       ans2.style.fontSize = "29px";
+       setTimeout(setback,5000);
+
+    }}}
+}
+    //Button clicking
     onclick("one",1)
     onclick("two",2)
     onclick("three",3)
@@ -45,11 +56,12 @@ function onclick(id,number){
     onclick("parenthesiso","(")
    onclick("parenthesisc",")")
     
+   //The Clear Button
    document.getElementById("clear").onclick = function(){
     document.getElementById("answer").textContent = "";
     exp = "";
    }
-
+//The equivalent of a Backspace
       document.getElementById("delete").onclick = function(){
     document.getElementById("answer").textContent = exp.slice(0,-1);
     exp = exp.slice(0,-1);
